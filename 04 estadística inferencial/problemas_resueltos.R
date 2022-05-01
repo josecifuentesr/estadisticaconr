0519bbcb9942294cd7b178699b7a54208f65a74f
josecifuentesr <josecifuentesr@gmail.com>
2022-04-30 21:19:24 -0400

    Se agregó el problema de más de dos muestras independientes

--- 04 estadística inferencial/problemas_resueltos.R
+++ 04 estadística inferencial/problemas_resueltos.R
@@ -1,11 +1,10 @@
 # Ejercicios resueltos unidad 4
 # Estos son los ejercicios que se encuentran en el documento PDF de la unidad
 
-# Prueba para dos proporciones
-# 2 muestras independientes
-
 ###############################################################################
 
+# Prueba para dos proporciones
+# 2 muestras independientes
 # Problema: Una mujer británica aficionada a tomar té con leche dice poder distinguir cuando se agrega primero a una taza la leche o el té. Para comprobarlo se le dan 8 tazas de té en donde 4 de ellas se agregó la leche primero. ¿Es cierta su afirmación de la mujer?
 
 # Los datos son los siguientes:
@@ -28,9 +27,9 @@ ggbarstats(data = as.data.frame(as.table(TeaTesting)),
            x = Guess, y = Truth, counts = Freq, 
            results.subtitle = FALSE, bf.message = FALSE)
 
-# La función ggbarstats() no indica los resultados de la prueba de Fischer. En esta oportunidad se usó solo para explorar los datos. 
+# La función ggbarstats() no indica los resultados de la prueba de Fischer. En esta oportunidad se usó solo para explorar los datos.  # nolint
 
-# En el gráfico se puede observar que la mujer acertó al 75% de las tazas donde se sirvió la leche primero y al 75% donde se sirvió primero el Té. 
+# En el gráfico se puede observar que la mujer acertó al 75% de las tazas donde se sirvió la leche primero y al 75% donde se sirvió primero el Té.  # nolint
 
 # Analizaremos si estos resultados son estadísticamente significativos aplicando la prueba de Fischer. 
 
@@ -43,6 +42,39 @@ CrossTable(x = TeaTesting,
 
 # NOTA: también podríamos haber considerado la hipótesis alternativa donde OR>1 (la mujer acierta con mayor probabilidad que por puro azar), en cuyo caso el resultado es el mismo,no es cierta la afirmación de la mujer.
 
+###############################################################################
+## Prueba para proporciones: más de dos muestras independientes
+## Se quiere comprobar si el voto en EEUU está determinado por el sexo. Para esto, se obtienen los resultados de una encuesta con intención de voto. Los resultados son los siguientes:
 
+M <- as.table(rbind(c(762, 327, 468), c(484,239,477)))
+dimnames(M) <- list(genero = c("Hombres", "Mujeres"),
+                    partido = c("Democrata", "Independiente", "Republicano"))
+M
 
+# Pasamos los datos a un dataframe para visualizarlos con la función ggbarstats
+library(ggplot2)
+library(ggstatsplot)
+ggbarstats(data = as.data.frame(M),
+           x = genero, y = partido, counts = Freq, bf.message = FALSE)
 
+# Como se quiere comparar la proporción de más de dos muestras independientes se ocupa la prueba Chi Cuadrado.
+# Se observa que existe una relación significativa entre el sexo y el tipo de voto X(2) = 30.07 , p < .0001, n = 2757$
+
+# Para evaluar si existen diferencias significativas en el porcentaje de mujeres que votan a cada partido político, debemos realizar comparaciones múltiples pareadas
+library(rstatix)
+pairwise_prop_test(M)
+
+# Se observan diferencias significativas en el grupo Republicanos respecto a los demás, donde las mujeres representan un mayor número de votantes.
+# A continuación, resolveremos el mismo problema a través de un método manual
+# Obtenemos la tabla de frecuencias y aplicamos la prueba Chi Cuadrado
+library(gmodels)
+CrossTable(M, expected = TRUE,
+           prop.r = FALSE, prop.t = FALSE,
+           prop.chisq = FALSE, chisq = TRUE, digits = 2)
+# Existe una relación significativa entre el sexo y el tipo de voto (X2(2)=30.07, p<.0001)
+
+# Estimamos el grado de asociación de las variables usando la V de Cramer
+library(vcd)
+assocstats(M)
+
+# Se observa que existe una relación débil con un V=0.104
