5b891f8c1a363970c6f416cbe3f4d5a8485829a5
josecifuentesr <josecifuentesr@gmail.com>
2022-05-05 09:37:25 -0400

    Problema 01 obligatorio U4 terminado

--- 04 estadistica inferencial/problemas_obligatorios/problema01.R
+++ 04 estadistica inferencial/problemas_obligatorios/problema01.R
@@ -1,2 +1,8 @@
+# Problema 01
+library(ggstatsplot)
 datos <- data.frame(name = c ("impuestos", "servicios"), count=c(624, 1200-624))
 ggpiestats(datos, x=name, counts = count)
+
+# Interpretación
+# Se observa x(1) = 1.02; p = 0.17; n = 1200
+# No se puede rechazar H0, por tanto, no se observa una diferencia significativa entre las preferencias de los encuestados. 
